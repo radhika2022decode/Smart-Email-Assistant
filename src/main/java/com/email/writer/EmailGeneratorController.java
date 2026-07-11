d397b64b42301850fb4a2ba1e557fc3c02e1a2bd
Radhika Gupta <guptaradhika2003@gmail.com>
2026-07-12 02:37:07 +0530

    enabled cross origin request

--- src/main/java/com/email/writer/EmailGeneratorController.java
+++ src/main/java/com/email/writer/EmailGeneratorController.java
@@ -1,6 +1,7 @@
 package com.email.writer;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import lombok.AllArgsConstructor;
 @RestController
 @RequestMapping("/api/email")
 @AllArgsConstructor
+@CrossOrigin(origins = "*")
 public class EmailGeneratorController {
 	
 	private final EmailGeneratorService emailGeneratorService;
